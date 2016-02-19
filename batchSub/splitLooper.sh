30661955404472c5b54db1a269827de9e706d5dd
awhitbeck <whitbeck.andrew@gmail.com>
2016-02-19 11:38:45 -0600

    minor adjustments to make sure that code runs for T5Wg samples

--- batchSub/splitLooper.sh
+++ batchSub/splitLooper.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-sample=""
+sample="SMS-T5Wg_mGl-1550_mNLSP-0to1500"
 
-for i in $(grep $sample ../src/inputFiles.txt)
+for i in $(grep $sample ../src/T5WgFiles.txt)
 do 
     tag=`echo $i | awk -F "/" '{print $NF}'`
     index=`echo $tag | awk -F "_" '{print $((NF-1)) }'`

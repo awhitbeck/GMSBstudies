b1e25f778645ac45d24d5276ab992fceb00d5590
awhitbeck <whitbeck.andrew@gmail.com>
2016-03-03 10:36:01 -0600

    fixing baselinePlots and adding jetIDpatch

--- src/baselinePlots.cc
+++ src/baselinePlots.cc
@@ -4,6 +4,8 @@
 #include "fillHisto.cc"
 #include "skim.cc"
 #include "weightProducer.cc"
+#include "producer.h"
+#include "jetIDpatch.cc"
 
 #include "TString.h"
 #include "TChain.h"
@@ -20,19 +22,19 @@ using namespace std;
 int main(int argc, char** argv){
 
   TString sample = argv[1];
-  TChain* t = buildChain("inputSkimFiles.txt",sample,"TreeMaker2/PreSelection");
+  TChain* t = buildChain("skimInput.txt",sample,"PreSelection");
 
-  cout << "RA2bNtuple" << endl;
   RA2bNtuple *ntuple = new RA2bNtuple(t);
-  cout << "weights" << endl;
-  weightProducer<RA2bNtuple> *wp = new weightProducer<RA2bNtuple>(t,"Weight",10.);
-  cout << "plot histo" << endl;
-  fillHisto<RA2bNtuple> *HThisto = new fillHisto<RA2bNtuple>(t,20,500,1500,sample+"_HT","HT",wp);
-  fillHisto<RA2bNtuple> *MHThisto = new fillHisto<RA2bNtuple>(t,20,0,1000,sample+"_MHT","MHT",wp);
-  fillHisto<RA2bNtuple> *NJetshisto = new fillHisto<RA2bNtuple>(t,6,3.5,10.5,sample+"_NJets","NJets",wp);
-  fillHisto<RA2bNtuple> *BTagshisto = new fillHisto<RA2bNtuple>(t,4,-0.5,2.5,sample+"_BTags","BTags",wp);
-  cout << "analyzer" << endl;
-  analyzer<RA2bNtuple> a<RA2bNtuple>(ntuple);
+  weightProducer<RA2bNtuple> *wp = new weightProducer<RA2bNtuple>(ntuple,"Weight",10.);
+  jetIDpatch<RA2bNtuple> *patch = new jetIDpatch<RA2bNtuple>(ntuple);
+  fillHisto<RA2bNtuple> *HThisto = new fillHisto<RA2bNtuple>(ntuple,20,500,1500,sample+"_HT","HT",wp);
+  fillHisto<RA2bNtuple> *MHThisto = new fillHisto<RA2bNtuple>(ntuple,20,0,1000,sample+"_MHT","MHT",wp);
+  fillHisto<RA2bNtuple> *NJetshisto = new fillHisto<RA2bNtuple>(ntuple,7,3.5,10.5,sample+"_NJets","NJets",wp);
+  fillHisto<RA2bNtuple> *BTagshisto = new fillHisto<RA2bNtuple>(ntuple,5,-0.5,4.5,sample+"_BTags","BTags",wp);
+
+  analyzer<RA2bNtuple> a(ntuple);
+
+  a.addProcessor( patch );
   a.addProcessor( wp );
   a.addProcessor( HThisto );
   a.addProcessor( MHThisto );
@@ -45,17 +47,17 @@ int main(int argc, char** argv){
   cout << "save tree" << endl;
   TFile* outFile = new TFile("signalRegionSkim_"+sample+".root","UPDATE");
 
-  skimmer->skimTree->Write();
-
-  select->histo->Write("baselineYields_"+sample);
-  outFile->Close();
 
   HThisto->histo->Write();
   MHThisto->histo->Write();
   NJetshisto->histo->Write();
   BTagshisto->histo->Write();
 
+  outFile->Close();
+
   delete ntuple;
+  delete wp; 
+  delete patch;
   delete HThisto;
   delete MHThisto;
   delete NJetshisto;
